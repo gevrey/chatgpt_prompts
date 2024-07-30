77f334f59f793a7ff464854deb14a52984b66483
Christophe Gevrey <gevrey@pobox.com>
2024-07-30 11:36:45 +0200

    Update prompts.user.js
    Refactor: Replace "doggeddog" with "gevrey" in ChatGPT Prompts user script
    
    - Updated namespace, author, resource, and require URLs to reflect the new author "gevrey"
    - Ensured all instances of "doggeddog" were replaced with "gevrey" for consistency

--- prompts.user.js
+++ prompts.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         ChatGPT Prompts
 // @license      GPL
-// @namespace    https://github.com/doggeddog
+// @namespace    https://github.com/gevrey
 // @version      0.2.6
 // @description  awesome chatGPT prompts
-// @author       doggeddog
-// @resource     IMPORTED_CSS https://github.com/doggeddog/tribute/raw/master/dist/tribute.css
-// @resource     jsonData https://github.com/doggeddog/chatgpt_prompts/raw/master/prompts.json
-// @require      https://github.com/doggeddog/tribute/raw/master/dist/tribute.js
+// @author       gevrey
+// @resource     IMPORTED_CSS https://github.com/gevrey/tribute/raw/master/dist/tribute.css
+// @resource     jsonData https://github.com/gevrey/chatgpt_prompts/raw/master/prompts.json
+// @require      https://github.com/gevrey/tribute/raw/master/dist/tribute.js
 // @match        https://chat.openai.com/*
 // @match        https://chatgpt.com/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
@@ -21,13 +21,9 @@ var userData = [
   {
     "key": "Spoken English Teacher and Improver",
     "prompt": "I want you to act as a spoken English teacher and improver. I will speak to you in English and you will reply to me in English to practice my spoken English. I want you to keep your reply neat, limiting the reply to 100 words. I want you to strictly correct my grammar mistakes, typos, and factual errors. I want you to ask me a question in your reply. Now let's start practicing, you could ask me a question first. Remember, I want you to strictly correct my grammar mistakes, typos, and factual errors.",
-    "title": "英语对话练习",
-    "prompt_cn": "我希望你能充当英语口语老师和提高者。我将用英语与你交谈，而你将用英语回答我，以练习我的英语口语。我希望你能保持回复的整洁，将回复限制在 100 字以内。我希望你能严格纠正我的语法错误、错别字和事实性错误。我希望你在回答中向我提出一个问题。现在我们开始练习，你可以先问我一个问题。记住，我要你严格纠正我的语法错误、错别字和事实性错误。",
-    "desc": "英语交谈对话，回复会限制在 100 字以内。输入中的语法错误、错别字和事实性错误将被纠正。",
     "tags": [
       "language"
     ],
-    "pinyin": "yingyuduihualianxi"
   },
 ]
 userData.push(...defaultData);
@@ -52,41 +48,6 @@ function load() {
     requireLeadingSpace: false
   });
   tribute.attach(textArea);
-
-  var tributeCN = new Tribute({
-    trigger: '#',
-    values: userData.filter(function (promp) {
-      return promp.prompt_cn;
-    }),
-    selectTemplate: function (item) {
-      if (typeof item === "undefined") return null;
-      if (item.original.prompt_cn) {
-        return item.original.prompt_cn;
-      } else {
-        return item.original.prompt;
-      }
-    },
-    menuItemTemplate: function (item) {
-      if (item.original.title) {
-        return item.original.title;
-      } else {
-        return item.original.key;
-      }
-
-    },
-    lookup: function (item, mentionText) {
-      if (!item.title) {
-        return item.key;
-      }
-      if (item.pinyin) {
-        return item.title + " " + item.pinyin;
-      } else {
-        return item.title;
-      }
-    },
-    requireLeadingSpace: false
-  });
-  tributeCN.attach(textArea);
 }
 
 (function () {
@@ -115,7 +76,6 @@ function load() {
     }
   });
   const config = { subtree: true, childList: true };
-  // start observing change
   observer.observe(document, config);
   setInterval(function () {
     load();
